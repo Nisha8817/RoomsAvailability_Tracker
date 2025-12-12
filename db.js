d6f409adb18674fe4da5765a1b134f3f12adff1b
Nisha Mandiwal <nishamandiwal786@gmail.com>
2025-12-12 23:41:51 +0530

    prepare for render deploy

--- db.js
+++ db.js
@@ -1,5 +1,11 @@
 const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("./database.sqlite");
+const path = require("path");
+
+// Always store SQLite in /data (Render uses this as writeable)
+const dbPath = path.join(__dirname, "data", "database.sqlite");
+
+const db = new sqlite3.Database(dbPath);
+
 
 db.serialize(() => {
     db.run(`
